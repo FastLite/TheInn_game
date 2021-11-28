e6c54d270c65247e07ea5eae372544bebddfe917
Vlad Volodin <Volodin_vlad2002@mail.ru>
2021-11-28 19:50:43 -0400

    animation

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -27,7 +27,7 @@ public class EndGame : MonoBehaviour
     // after 25 seconds, game returns back to main menu
     IEnumerator BackToMenu()
     {
-        yield return new WaitForSeconds(25f);
+        yield return new WaitForSeconds(35f);
         SceneManager.LoadScene(0);
     }
 
